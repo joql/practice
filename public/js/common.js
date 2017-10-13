860464ec89a94c9faf42a54b8df81f3aef3243d4
joql <981037735@qq.com>
2017-10-13 17:37:28 +0800

    feat test

---
+++ public/js/common.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2017/10/13.
+ */
+
+//json格式判断
+function isJSON(str) {
+    if (typeof str == 'string') {
+        try {
+            JSON.parse(str);
+            return true;
+        } catch(e) {
+            return false;
+        }
+    }else{
+        return false;
+    }
+}
+
+//json高亮
+function highLight(json){
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
