e4e1c55fafb5d998fc9141f7641a4e771b63c142
joql <981037735@qq.com>
2018-01-26 16:44:23 +0800

    fix php-resque 队列

--- demo/php-resque/job.php
+++ demo/php-resque/job.php
@@ -5,11 +5,16 @@
  * Date: 2018/1/26
  * Time: 15:29
  */
+require '/init.php';
 class PHP_Job
 {
     public function perform()
     {
-        sleep(120);
-        fwrite(STDOUT, 'Hello!');
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('your.log', Logger::WARNING));
+
+        // add records to the log
+        $log->warning('Foo');
+        $log->error('Bar');
     }
 }
