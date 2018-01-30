0d3c69b5e353fee3445c8d9ae0917f0018cb28d7
joql <981037735@qq.com>
2018-01-30 16:59:06 +0800

    fix websocket

--- demo/swoole/websocket/room.php
+++ demo/swoole/websocket/room.php
@@ -6,22 +6,55 @@
  * Time: 10:38
  */
 
-$ws = new swoole_websocket_server('0.0.0.0',9502);
-$ws->user_c = [];
+class Imsock
+{
+    private $_user_list = array();
+    private $_server = null;
 
-$ws->on('open',function ($ws,$requset){
-    $ws->user_c[]=$requset->id;
-});
+    public function __construct($ip_address='192.168.19.128', $ip_port=9502)
+    {
+        echo "swoole_websocket 服务开启\n";
+        $this->_server = new swoole_websocket_server($ip_address, $ip_port);
 
-$ws->on('message', function ($ws,$frame){
-    $msg = 'from'.$frame->fd.":{$frame->data}\n";
-    foreach ($ws->user_c as $v){
-        $ws->push($v,$msg);
+        $this->_server->on('open', function($server, $request)
+        {
+            $this->_sopen( $server, $request );
+            echo "用户 {$request->fd} 连接服务器\n";
+        });
+
+        $this->_server->on('message', function($server, $frame)
+        {
+            $this->_sendmsg( $server, $frame );
+        });
+
+        $this->_server->on('close', function($server, $fd)
+        {
+            $this->_sclose( $server, $fd );
+        });
+
+        $this->_server->start();
     }
-});
 
-$ws->on('close',function ($ws,$fd){
-    unset($ws->user_c['$fd-1']);
-});
+    private function _sopen($server, $request)
+    {
+        $this->_user_list[ $request->fd ] = 0;
+    }
 
-$ws->start();
+    // 给客户端发送信息
+    private function _sendmsg(swoole_websocket_server $server, $frame)
+    {
+        $msg = "from {$frame->fd}: {$frame->data}\n";
+        foreach ($this->_user_list as $k=>$v){
+            $server->push($k,$msg);
+            echo "用户 {$k} 推送成功\n";
+        }
+    }
+
+    private function _sclose( swoole_websocket_server $server, $fd )
+    {
+        unset( $this->_user_list[$fd] );
+        echo "用户 $fd  退出登录\n";
+    }
+}
+
+$Imsock = new Imsock();
