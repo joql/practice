2a95d94d638ed189b1972fa57cf7787d13cbced9
joql <981037735@qq.com>
2018-06-13 14:05:48 +0800

    fix qiniu fetcht抓取第三方图片

--- demo/qiniu/auth.php
+++ demo/qiniu/auth.php
@@ -8,7 +8,7 @@
 
 require '../../init.php';
 
-use Qiniu\Auth;
+
 use GuzzleHttp\Client;
 error_reporting(E_ALL);;
 
@@ -16,28 +16,48 @@ error_reporting(E_ALL);;
 $accessKey = '';
 $secretKey = '';
 
-$auth = new Auth($accessKey, $secretKey);
+$qiniu = new QiNiu($accessKey, $secretKey);
+$data = $qiniu->fetch('blog','http://fanyi.baidu.com/static/webpage/img/download/spritesheet.png');
 
-
-
-
-$EncodedURL = \Qiniu\base64_urlSafeEncode('http://fanyi.baidu.com/static/webpage/img/download/spritesheet.png');
-$EncodedEntryURI = \Qiniu\base64_urlSafeEncode('blog');
-$parm = "/fetch/$EncodedURL/to/$EncodedEntryURI";
-$token = $auth->sign("$parm\n");
-
-
-echo 'EncodedURL = '.$EncodedURL."<br />";
-echo 'EncodedEntryURI = '.$EncodedEntryURI."<br />";
-echo 'token = '.$token."<br />";
 
 $client = new Client();
-$response = $client->get("http://iovip-z1.qbox.me$parm",[
+$response = $client->get("http://iovip-z1.qbox.me".$data[0],[
     'headers'=>[
-        'Authorization' =>"QBox $token"
+        'Authorization' =>"QBox ".$data[1]
     ]
 ]);
 
 $result = $response->getBody();
 echo $result;
 /*{"fsize":34329,"hash":"Fkf3l424A8kf681zo_fxmWtGq8QO","key":"Fkf3l424A8kf681zo_fxmWtGq8QO","mimeType":"image/png"}*/
+
+class QiNiu
+{
+    public $accessKey;
+    public $secretKey;
+
+    public function __construct($accessKey, $secretKey){
+        $this->accessKey = $accessKey;
+        $this->secretKey = $secretKey;
+    }
+
+    public function fetch($bucket, $pic_url){
+        $EncodedURL = $this->base64SafeEncode($pic_url);
+        $EncodedEntryURI = $this->base64SafeEncode($bucket);
+        $parm = "/fetch/$EncodedURL/to/$EncodedEntryURI";
+        $token = $this->sign("$parm\n");
+        return [$parm,$token];
+    }
+
+    public function sign($data){
+        $hmac = hash_hmac('sha1', $data, $this->secretKey, true);
+        return $this->accessKey . ':' . $this->base64SafeEncode($hmac);
+    }
+
+    private function base64SafeEncode($data){
+        $find = array('+', '/');
+        $replace = array('-', '_');
+        return str_replace($find, $replace, base64_encode($data));
+    }
+
+}
